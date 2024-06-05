dcf3b04861c4e5a94179aff1a701e0e739dba21c
darel919 <darrell.cristanto@gmail.com>
2024-06-06 03:51:24 +0700

    change to https

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -43,7 +43,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "52a3222da32e8378b23ec16dca8dabba
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "firebase-messaging-sw.js": "08d71844e18c4b4ee88b30a2479bc870",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "c5c73abb6acb975a7662b79508d4a84f",
+"flutter_bootstrap.js": "2f25c3a09d6678b8b6f7cc27af660a5a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
