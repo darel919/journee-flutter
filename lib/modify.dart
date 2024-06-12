f0c4ab281d21ea7bc0efae6cf99b6c1713309287
darel919 <darrell.cristanto@gmail.com>
2024-06-12 22:09:35 +0700

    added http client for web instead of dio

--- lib/modify.dart
+++ lib/modify.dart
@@ -18,6 +18,7 @@ import 'package:image_compression_flutter/image_compression_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:flutter_pannable_rating_bar/flutter_pannable_rating_bar.dart';
 import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
 
 class CreateDiaryPage extends StatefulWidget {
   const CreateDiaryPage({super.key});
@@ -1047,9 +1048,67 @@ class _CreateDiaryPageState extends State<CreateDiaryPage> {
           setState(() {
             isAILoading = true;
           });
-          // if(kIsWeb) {
+          if(kIsWeb) {
+            final response = await http.post(
+              Uri.parse(dotenv.env['supabaseSelfHostUrl']!+':2024/v1/chat/completions'),
+              headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*', // Set to your domain or '*'
+              },
+              body: jsonEncode({
+                "model": "gpt-3.5-turbo",
+                "messages": [
+                  {
+                    "role": "system",
+                    "content": 'You are an AI Assistant, your task is to organize all the text inputs to separate lists. These are about food reviews. I want you to separate the reviews and the food prices aside. The text prompt for example would be something like this: "Bakmi PG, Alkid, Yogyakarta Ini adalah salah satu Bakmi Jawa Goreng terenak, beda tipis sama yang di Rama Shinta. Kita sengaja kebawah buat cobain ini. Lokasinya ditengah kota, tepatnya deket Alkid. Parkirnya agak susah karena tempatnya kecil tapi parkir gratis. Buat Bakmi Jawa Gorengnya harganya 28k. Agak pricey sih tapi untung rasanya enak banget.Kalo buat minumnya pesen Es Teh Manis, harganya 7k. Tehnya wangi banget, enak juga rasanya. Menurutku, ini recommended kalo lagi kebawah (dan lagi banyak uang)" From this text prompt, you should return two prompts. The first one is rewrite all the text prompt you receive so it sounds like it was written by a food reviewer.The second one is like this: "Bakmi Jawa Goreng: Rp28.000 Es Teh Manis: Rp7.000" k means thousand, so 10k is 10000. But since we are talking about prices in Indonesian currency (Rupiah), so you should translate it to that. Please provide the following information as a JSON object: - food_prices: - Item: Bakmi Jawa Goreng - Price: Rp28.000 - ai_caption: "Ini adalah salah satu Bakmi Jawa Goreng terenak yang ditemukan di Yogyakarta. Lokasinya di tengah kota, dekat Alkid, dan parkiran gratis. Bakmi Jawa Gorengnya dihargai sekitar Rp28.000, sedangkan Es Teh Manis sekitar Rp7.000. Rasanya enak banget, kalo lagi kebawah." DO NOT return anymore word like Here is the response in JSON format. JUST RETURN THE JSON."',
+                  },
+                  {
+                    "role": "user",
+                    "content": myController.text
+                  }
+                ]
+              }),
+            );
 
-          // } else {
+            if (response.statusCode == 200) {
+              // Handle success
+              print(response.body);
+              Map<String, dynamic> aiSortedResponse = jsonDecode(response.body);
+              Map<String, dynamic> aiSortedData = aiSortedResponse['choices'][0]['message']['content'];
+              foodPrices = aiSortedData['food_prices'];
+
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('AI Rewrite success!'),
+                  elevation: 20.0,
+                ),
+              );
+              setState(() {
+                AITextControls.value = TextEditingValue(
+                  text: aiSortedData['ai_caption'],
+                  selection: TextSelection.fromPosition(TextPosition(offset: aiSortedData['ai_caption'].length)),
+                );
+                isAILoading = false;
+                isAIDone = true;
+                viewTextMode = 'ai';
+              });
+            } else {
+              // Handle error
+              setState(() {
+                isAILoading = false;
+                isAIDone = false;
+                viewTextMode = 'user';
+              });
+              var error = response.reasonPhrase;
+              ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('Unfortunately, AI is currently not available. $error'),
+                elevation: 20.0,
+              ),
+            );
+            }
+
+          } else {
             var response = await dio.request(
             dotenv.env['supabaseSelfHostUrl']!+':2024/v1/chat/completions', 
             options: Options(
@@ -1088,7 +1147,7 @@ class _CreateDiaryPageState extends State<CreateDiaryPage> {
             isAIDone = true;
             viewTextMode = 'ai';
           });
-          // }
+          }
         } on DioException catch (e) {
           setState(() {
             isAILoading = false;
